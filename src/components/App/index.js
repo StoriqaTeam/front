fe2f7d8892a617e8b8722abc3c576d02828caba0
saldin <saldin.iliya@gmail.com>
2017-12-07 18:08:48 +0300

    Add babel-module-resolver.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
-// import './index.css';
-
 class App extends PureComponent {
   static defaultProps = {
     someData: 'test',
