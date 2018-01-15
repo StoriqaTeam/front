3755dfbeb43d3c1e7696ef012a51248fcb3ca837
aneurysmjs <aneurysmjs@outlook.com>
2018-01-15 07:29:21 -0500

    docs (StoryBook) add stories to 'Button' component

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import Button from 'components/Button';
+import Button from './Button';
 
 storiesOf('Button', module)
   .add('with text', () => (
@@ -10,4 +10,12 @@ storiesOf('Button', module)
   .add('with some emoji', () => (
     // eslint-disable-next-line
     <Button title="😀 😎 👍 💯" />
+  ))
+  .add('with "buttonClass" as default', () => (
+    // eslint-disable-next-line
+    <Button buttonClass="Button" title="Storiqa" />
+  ))
+  .add('with "buttonClass" as "SignUpFormButton" ', () => (
+    // eslint-disable-next-line
+    <Button buttonClass="SignUpFormButton" title="Storiqa" />
   ));
