49b426b0b6d153be56924a5e9afd12f058a32877
Saldin Ilya <saldin.iliya@gmail.com>
2018-01-21 12:29:28 +0300

    Simplify run with docker [2].

--- scripts/docker.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-fs.stat('.env.docker', (err, stat) => {
-  if (stat && stat.isFile()) {
-    console.log('Renaming .env.docker to .env.development');
-    fs.rename('.env.docker', '.env.development', (err) => console.log(err));
-  }
-});
