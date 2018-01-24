b51dcdfbd0e6024ec6ad28257ddff6d9f48fa3e9
saldin <saldin.iliya@gmail.com>
2018-01-24 13:00:01 +0300

    Extract oauth-vars to .env

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -56,16 +56,19 @@ class Login extends Component<PropsType, StateType> {
 
   facebookLoginString = () => {
     // $FlowIgnore
-    const appId = `${process.env.REACT_APP_FACEBOOK_APP_ID}`;
-    const redirectUri = 'http://localhost:3003/oauth_callback/fb';
+    const appId = `${process.env.REACT_APP_OAUTH_FACEBOOK_APP_ID}`;
+    // $FlowIgnore
+    const redirectUri = `${process.env.REACT_APP_OAUTH_FACEBOOK_REDIRECT_URI}`;
     return `https://www.facebook.com/v2.11/dialog/oauth?client_id=${appId}&redirect_uri=${redirectUri}&scope=email,public_profile`;
   };
 
   googleLoginString = () => {
     // $FlowIgnore
-    const appId = `${process.env.REACT_APP_GOOGLE_CLIENT_ID}`;
-    const redirectUri = 'http://localhost:3003/oauth_callback/google';
-    const scopes = 'https://www.googleapis.com/auth/userinfo.email+https://www.googleapis.com/auth/userinfo.profile';
+    const appId = `${process.env.REACT_APP_OAUTH_GOOGLE_CLIENT_ID}`;
+    // $FlowIgnore
+    const redirectUri = `${process.env.REACT_APP_OAUTH_GOOGLE_REDIRECT_URI}`;
+    // $FlowIgnore
+    const scopes = `${process.env.REACT_APP_OAUTH_GOOGLE_SCOPES}`;
     return `https://accounts.google.com/o/oauth2/v2/auth?client_id=${appId}&redirect_uri=${redirectUri}&scope=${scopes}&response_type=code`;
   };
 
