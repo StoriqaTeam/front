65d89b2bcdbcf6748402f6998c7c0fec73021b60
il.ya sald.in <saldin.iliya@gmail.com>
2018-02-16 17:08:35 +0300

    Interim.

---
+++ src/components/App/Header/Header.js
@@ -0,0 +1,21 @@
+// @flow
+
+import React, { PureComponent } from 'react';
+
+import './Header.scss';
+
+type PropsType = {
+  //
+};
+
+class Header extends PureComponent<PropsType> {
+  render() {
+    return (
+      <div styleName="container">
+        fasdfas
+      </div>
+    );
+  }
+}
+
+export default Header;
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,2 +1,3 @@
 // eslint-disable-next-line
 export { default as App } from './App';
+export { default as Header } from './Header/Header';
