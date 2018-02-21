3bdc7523c1ad9d184644ebf5361a6b99dd76efea
Aleksey Levenets <a.levenec@storiqa.com>
2018-02-21 13:12:46 +0300

    add textarea

--- src/components/Forms/index.js
+++ src/components/Forms/index.js
@@ -1,3 +1,4 @@
 export { default as Input } from './Input';
+export { default as Textarea } from './Textarea';
 export { default as Checkbox } from './Checkbox';
 export { default as RadioGroup } from './RadioGroup';
