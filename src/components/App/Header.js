543d1e19cee967d1c7075dfa376edb55a25e737b
Aleksey Levenets <a.levenec@storiqa.com>
2018-02-22 16:54:26 +0300

    add top block in header

--- src/components/App/Header.js
+++ src/components/App/Header.js
@@ -6,6 +6,7 @@ import { SearchInput } from 'components/SearchInput';
 import { ProfileMenu } from 'components/ProfileMenu';
 import { CartButton } from 'components/CartButton';
 import { Button } from 'components/Button';
+import { MiniSelect } from 'components/MiniSelect';
 
 import { Container, Row, Col } from 'layout';
 
@@ -26,7 +27,57 @@ class Header extends PureComponent<PropsType> {
         <Container>
           <Row>
             <Col size={12}>
-              <div styleName="wrap">
+              <div styleName="top">
+                <div styleName="item">
+                  <MiniSelect
+                    label="Dropdown"
+                    items={[
+                      { id: '1', label: 'BTC' },
+                      { id: '2', label: 'ETH' },
+                      { id: '3', label: 'STQ' },
+                      { id: '4', label: 'ADA' },
+                      { id: '5', label: 'NEM' },
+                      { id: '6', label: 'STRAT' },
+                    ]}
+                  />
+                </div>
+                <div styleName="item">
+                  <MiniSelect
+                    label="Dropdown"
+                    items={[
+                      { id: '1', label: 'ENG' },
+                      { id: '2', label: 'CHN' },
+                      { id: '3', label: 'RUS' },
+                    ]}
+                  />
+                </div>
+                <div styleName="item">
+                  <MiniSelect
+                    isDropdown
+                    label="Terms & Conditions"
+                    items={[
+                      { id: '1', label: 'Punkt #1' },
+                      { id: '2', label: 'Punkt #2' },
+                      { id: '3', label: 'Punkt #3' },
+                    ]}
+                  />
+                </div>
+                <div styleName="item">
+                  <MiniSelect
+                    isDropdown
+                    label="Delivery"
+                    items={[
+                      { id: '1', label: 'Punkt #1' },
+                      { id: '2', label: 'Punkt #2' },
+                      { id: '3', label: 'Punkt #3' },
+                    ]}
+                  />
+                </div>
+                <div styleName="item">
+                  <a href="/">Quality Assurance</a>
+                </div>
+              </div>
+              <div styleName="bottom">
                 <div styleName="logo">
                   <Logo styleName="logo" />
                 </div>
