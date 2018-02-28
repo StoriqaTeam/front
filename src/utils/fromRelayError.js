ce43500e0cdeeede8975a89dec671f441f8c369f
il.ya sald.in <saldin.iliya@gmail.com>
2018-02-28 18:38:05 +0300

    Add messages to `fromRelayError` parser.

--- src/utils/fromRelayError.js
+++ src/utils/fromRelayError.js
@@ -1,12 +1,25 @@
 // @flow
 
-import { fromPairs, map, pathOr, pipe, prop } from 'ramda';
+import { map, pathOr, pipe, prop, head, fromPairs, toString } from 'ramda';
 
+/*
+  see test
+*/
 export default pipe(
   pathOr([], ['source', 'errors']),
   map((item) => {
     const error = prop('data', item);
-    return [prop('code', error), pathOr('', ['details', 'status'], error)];
+    const code = prop('code', error);
+    const status = pathOr('', ['details', 'status'], error);
+    const messageRaw = pathOr('', ['details', 'message'], error);
+    const message = map(pipe(
+      head,
+      prop('message'),
+    ))(JSON.parse(messageRaw));
+    return [toString(code), {
+      status,
+      message,
+    }];
   }),
   fromPairs,
 );
