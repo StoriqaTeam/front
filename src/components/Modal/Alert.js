7dc4f627302856eb19640b0bc9cd6990a25aed3d
Aleksey Levenets <a.levenec@storiqa.com>
2018-02-28 17:16:31 +0300

    add alert

---
+++ src/components/Modal/Alert.js
@@ -0,0 +1,74 @@
+// @flow
+
+import React, { Component } from 'react';
+import classNames from 'classnames';
+
+import { Icon } from 'components/Icon';
+
+import Portal from './Portal';
+import Sad from './svg/sad.svg';
+import Merry from './svg/merry.svg';
+
+import './Alert.scss';
+
+type PropsTypes = {
+  showAlert: ?boolean,
+  text: string,
+  isError: ?boolean,
+};
+
+type StateTypes = {
+  showAlert: ?boolean,
+};
+
+class Modal extends Component<PropsTypes, StateTypes> {
+  state = {
+    showAlert: false,
+  };
+
+  componentWillMount() {
+    this.setState({ showAlert: this.props.showAlert });
+  }
+
+  componentWillReceiveProps(nextProps: Object) {
+    const { showAlert } = nextProps;
+
+    if (this.props.showAlert !== showAlert) {
+      this.setState({ showAlert });
+    }
+  }
+
+  onCloseModal = () => {
+    this.setState({ showAlert: false });
+  };
+
+  render() {
+    if (!this.state.showAlert) return null;
+
+    const { text, isError } = this.props;
+
+    return (
+      <Portal>
+        <div styleName={classNames('container', { isError })}>
+          <div styleName="wrap">
+            <div styleName="inner">
+              {isError ? <Sad styleName="icon" /> : <Merry styleName="icon" />}
+              <div styleName="text">{text}</div>
+              <div
+                styleName="cross"
+                onClick={this.onCloseModal}
+                onKeyDown={() => {}}
+                role="button"
+                tabIndex="0"
+              >
+                <Icon type="crossWhite" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </Portal>
+    );
+  }
+}
+
+export default Modal;
