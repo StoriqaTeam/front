34933796f2065c8b12462d33d7fab7f9051425d1
aneurysmjs <aneurysmjs@outlook.com>
2018-04-03 16:43:11 +0300

    feat (ProductThumbnails) add thumbnails props

--- src/pages/Store/Product/ProductImage.js
+++ src/pages/Store/Product/ProductImage.js
@@ -6,12 +6,28 @@ import { ProductThumbnails } from 'pages/Store/Product';
 
 import './ProductImage.scss';
 
-class ProductImage extends PureComponent<{}> {
-  handleLightBox = {};
+type stateTypes = {
+  thumbnails: {img: string, alt: string}[],
+}
+
+class ProductImage extends PureComponent<{}, stateTypes> {
+  state = {
+    thumbnails: [
+      {
+        src: 'https://www.flightclub.com/media/catalog/product/cache/1/image/1600x1140/9df78eab33525d08d6e5fb8d27136e95/8/0/801640_1.jpg',
+        alt: 'air jordan sideways',
+      },
+      {
+        src: 'https://www.studio-88.co.za/wp-content/uploads/2017/10/NIKE-JORDAN-AIR-JORDAN-1-MID-BLACK-BLACK-NKK961BP-V4.jpg',
+        alt: 'air jordan front',
+      },
+    ],
+  };
   render() {
+    const { thumbnails } = this.state;
     return (
       <div styleName="container">
-        <ProductThumbnails />
+        <ProductThumbnails thumbnails={thumbnails} />
         <img
           src="https://www.studio-88.co.za/wp-content/uploads/2017/10/NIKE-JORDAN-AIR-JORDAN-1-MID-BLACK-BLACK-NKK961BP-V4.jpg"
           alt="nike air jordan"
--- src/pages/Store/Product/ProductThumbnails.js
+++ src/pages/Store/Product/ProductThumbnails.js
@@ -4,15 +4,19 @@ import React, { PureComponent } from 'react';
 
 import './ProductThumbnails.scss';
 
-class ProductThumbnails extends PureComponent<{}> {
+type propsTypes = {
+  thumbnails: {img: string, alt: string}[],
+}
+
+class ProductThumbnails extends PureComponent<propsTypes> {
   handleLightBox = {};
   render() {
+    const { thumbnails } = this.props;
     return (
       <div styleName="container">
-        <img
-          src="https://www.studio-88.co.za/wp-content/uploads/2017/10/NIKE-JORDAN-AIR-JORDAN-1-MID-BLACK-BLACK-NKK961BP-V4.jpg"
-          alt="nike air jordan"
-        />
+        {thumbnails.map(({ src, alt }) => (
+          <img src={src} alt={alt} />
+        ))}
       </div>
     );
   }
