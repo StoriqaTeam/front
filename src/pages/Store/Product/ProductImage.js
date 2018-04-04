06ddba7ab2c6fe001b1ea82d8b9b1b6e0e418821
aneurysmjs <aneurysmjs@outlook.com>
2018-04-04 12:31:56 +0300

    feat (ProductThumbnails) revert back as a class since apply border on a clicked img

--- src/pages/Store/Product/ProductImage.js
+++ src/pages/Store/Product/ProductImage.js
@@ -17,18 +17,22 @@ class ProductImage extends PureComponent<{}, stateTypes> {
   state = {
     thumbnails: [
       {
+        id: 0,
         src: 'https://www.flightclub.com/media/catalog/product/cache/1/image/1600x1140/9df78eab33525d08d6e5fb8d27136e95/8/0/801640_1.jpg',
         alt: 'air jordan sideways',
       },
       {
+        id: 1,
         src: 'https://www.studio-88.co.za/wp-content/uploads/2017/10/NIKE-JORDAN-AIR-JORDAN-1-MID-BLACK-BLACK-NKK961BP-V4.jpg',
         alt: 'air jordan front',
       },
       {
+        id: 2,
         src: 'https://cdn.thesolesupplier.co.uk/2017/09/Nike-Air-Jordan-1-Mid-Triple-Black-03.jpeg',
         alt: 'air jordan back',
       },
       {
+        id: 3,
         src: 'https://images.ua.prom.st/935779920_w640_h640_air_jordan_1_m__01_696x489.jpg',
         alt: 'air jordan back right',
       },
--- src/pages/Store/Product/ProductThumbnails.js
+++ src/pages/Store/Product/ProductThumbnails.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 
 import './ProductThumbnails.scss';
 
@@ -8,18 +8,46 @@ type propsTypes = {
   thumbnails: {img: string, alt: string}[],
 }
 
-const ProductThumbnails = (props: propsTypes) => {
-  const { thumbnails } = props;
-  return (
-    <div styleName="container">
-      {thumbnails.map(({ src, alt }) => (
-        <img
-          src={src}
-          alt={alt}
-        />
-      ))}
-    </div>
-  );
-};
+type stateTypes = {
+  isClicked: number
+}
+
+class ProductThumbnails extends Component<propsTypes, stateTypes> {
+  state = {
+    clicked: null,
+  };
+  /**
+   * Highlights img's border when clicked
+   * @param {number} index
+   * @return {void}
+   */
+  handleClick = (index: number): void => {
+    this.setState({
+      clicked: index,
+    });
+  };
+  render() {
+    const { thumbnails } = this.props;
+    const { clicked } = this.state;
+    return (
+      <div styleName="container">
+        {thumbnails.map(({ id, src, alt }, index) => (
+          <button
+            key={id}
+            onClick={() => this.handleClick(index)}
+          >
+            <figure>
+              <img
+                styleName={clicked === index ? 'clicked' : ''}
+                src={src}
+                alt={alt}
+              />
+            </figure>
+          </button>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default ProductThumbnails;
