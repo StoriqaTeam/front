50f9097d87a687eb3396a0b2ffef634d0fe98424
aneurysmjs <aneurysmjs@outlook.com>
2018-04-04 11:35:51 +0300

    refactor (ProductThumbnails) convert it as a functional stateless component

--- src/pages/Store/Product/ProductThumbnails.js
+++ src/pages/Store/Product/ProductThumbnails.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React, { PureComponent } from 'react';
+import React from 'react';
 
 import './ProductThumbnails.scss';
 
@@ -8,18 +8,18 @@ type propsTypes = {
   thumbnails: {img: string, alt: string}[],
 }
 
-class ProductThumbnails extends PureComponent<propsTypes> {
-  handleLightBox = {};
-  render() {
-    const { thumbnails } = this.props;
-    return (
-      <div styleName="container">
-        {thumbnails.map(({ src, alt }) => (
-          <img src={src} alt={alt} />
-        ))}
-      </div>
-    );
-  }
-}
+const ProductThumbnails = (props: propsTypes) => {
+  const { thumbnails } = props;
+  return (
+    <div styleName="container">
+      {thumbnails.map(({ src, alt }) => (
+        <img
+          src={src}
+          alt={alt}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default ProductThumbnails;
