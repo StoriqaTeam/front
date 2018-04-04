9fcda780dc19a8d42ad81524bb0adaab438b00fc
Alexander Korovkin <greenteamer@bk.ru>
2018-04-04 11:47:25 +0300

    add ranger library & fix lint

--- src/components/Ranger/Ranger.js
+++ src/components/Ranger/Ranger.js
@@ -1,21 +1,54 @@
 // @flow
 
 import React from 'react';
+import { RangerSlider } from './lib';
+// import 'react-rangeslider/lib/index.css';
 
 // import AccordionBlock from './AccordionBlock';
 
 import './Ranger.scss';
 
+
 type PropsType = {
-  onStartChange?: (value: number) => void,
-  onEndChange?: (value: number) => void,
+  // onStartChange?: (value: number) => void,
+  // onEndChange?: (value: number) => void,
 }
 
-const Ranger = () => (
-  <div styleName="wrapper">
-    Hello Ranger
-  </div>
-);
+type StateType = {
+  volume: number,
+}
+
+class Ranger extends React.Component<PropsType, StateType> {
+  constructor(props: PropsType) {
+    super(props);
+    this.state = {
+      volume: 0,
+    };
+  }
+
+  handleOnChange = (volume: number) => {
+    this.setState({
+      volume,
+    });
+  }
+
+  render() {
+    const { volume } = this.state;
+    return (
+      <div styleName="wrapper">
+        <div styleName="rangerContainer">
+          <RangerSlider
+            value={volume}
+            orientation="vertical"
+            onChange={this.handleOnChange}
+          />
+          <div styleName="leftControll" />
+          <div styleName="rightControll" />
+        </div>
+      </div>
+    );
+  }
+}
 
 
 export default Ranger;
