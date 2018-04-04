622d43285ceb7b1078390e53c159f4846fbc7f0b
Alexander Korovkin <greenteamer@bk.ru>
2018-04-04 19:02:48 +0300

    styles done

--- src/components/Ranger/Ranger.js
+++ src/components/Ranger/Ranger.js
@@ -2,10 +2,6 @@
 
 import React from 'react';
 import { RangerSlider } from './lib';
-import Slider from 'react-rangeslider';
-// import 'react-rangeslider/lib/index.css';
-
-// import AccordionBlock from './AccordionBlock';
 
 import './Ranger.scss';
 
@@ -24,8 +20,8 @@ class Ranger extends React.Component<PropsType, StateType> {
   constructor(props: PropsType) {
     super(props);
     this.state = {
-      volume: 0,
-      volume2: 0,
+      volume: 2,
+      volume2: 4.005,
     };
   }
 
@@ -37,28 +33,23 @@ class Ranger extends React.Component<PropsType, StateType> {
 
   render() {
     const { volume, volume2 } = this.state;
-    console.log('***** value: ', { volume, volume2 });
     return (
       <div styleName="wrapper">
         <div styleName="rangerContainer">
           <RangerSlider
             min={0}
-            max={1000}
+            max={10}
+            step={0.0001}
             value={volume}
             value2={volume2}
             orientation="horizontal"
             onChange={value => this.handleOnChange(value, 'volume')}
             onChange2={value => this.handleOnChange(value, 'volume2')}
           />
-          {/* <Slider
-            min={0}
-            max={100}
-            value={volume}
-            orientation="horizontal"
-            onChange={this.handleOnChange}
-          /> */}
-          <div styleName="leftControll">{volume}</div>
-          <div styleName="rightControll">{volume2}</div>
+          <div styleName="valuesContainer">
+            <div styleName="leftValue value">{volume}</div>
+            <div styleName="rightValue value">{volume2}</div>
+          </div>
         </div>
       </div>
     );
