8a19f085d50ef7c59baa291761581b02c47ec8b2
Levenets Aleksey <a.levenec@storiqa.com>
2018-04-05 12:48:03 +0300

    fix design bugs

--- src/components/Slider/SliderHeader.js
+++ src/components/Slider/SliderHeader.js
@@ -20,9 +20,7 @@ class SliderHeader extends PureComponent<PropsTypes> {
 
     return (
       <div styleName="container">
-        <div styleName="title">
-          <strong>{title}</strong>
-        </div>
+        <div styleName="title">{title}</div>
         {isRevealButton &&
           <div styleName="nav">
             <button
