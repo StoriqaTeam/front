931258b2855a64cf01e0ac6851ad54c7570d7fd9
aneurysmjs <aneurysmjs@outlook.com>
2018-04-06 16:42:29 +0300

    feat (ProductMaterials) pass 'onSelect' prop and apply h4 styles

--- src/pages/Store/Product/ProductMaterial.js
+++ src/pages/Store/Product/ProductMaterial.js
@@ -11,17 +11,19 @@ type material = {id: string | number, label: string};
 type propTypes = {
   selected: material,
   materials: material[],
+  onSelect: Function,
 }
 
 const ProductMaterial = (props: propTypes) => (
   <div styleName="container">
-    <h4 styleName="title">
+    <h4>
       Материал
     </h4>
     <MiniSelect
       forForm
       activeItem={props.selected}
       items={props.materials}
+      onSelect={props.onSelect}
     />
   </div>
 );
