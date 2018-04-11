6811b5d6548ebb82e0748ce056b91b0b3cfa54c5
il.ya sald.in <saldin.iliya@gmail.com>
2018-04-11 16:13:07 +0300

    Fixes

--- src/utils/extractPhotos.js
+++ src/utils/extractPhotos.js
@@ -9,9 +9,7 @@ import { has, isEmpty } from './index';
  */
 export default function extractPhotos(array: []): [] {
   // console.log('array', array);
-  const photos = array.filter((variant) => {
-    return has(variant, 'product');
-  }).map(p => p.product);
+  const photos = array.filter(variant => has(variant, 'product')).map(p => p.product);
   return photos.reduce((p) => {
     const additionalPhotos = !isEmpty(p.product.addtionalPhotos) ? p.product.addtionalPhotos : [];
     return {
