d6eaa309ca2c9199085c59816584f898eb1bdab0
Saldin Ilya <saldin.iliya@gmail.com>
2018-04-16 18:14:07 +0300

    Ilya/hotfix/login (#104)
    * Hotfix
    
    * Fix CI errors.
    
    * Hotfix

--- src/components/Authorization/Authorization.js
+++ src/components/Authorization/Authorization.js
@@ -104,9 +104,9 @@ class Authorization extends Component<PropsType, StateType> {
   handleLoginClick = () => {
     this.setState({ isLoad: true, errors: null });
     const { alone } = this.props;
-    const { username, password } = this.state;
+    const { email, password } = this.state;
     GetJWTByEmailMutation.commit({
-      email: username,
+      email,
       password,
       environment: this.context.environment,
       onCompleted: (response: ?Object, errors: ?Array<Error>) => {
