e09af2a1871a1c85848220c9fc8fcb0206a2d268
Saldin Ilya <saldin.iliya@gmail.com>
2018-04-27 22:53:56 +0300

    commit with flow error

--- src/components/AddressAutocomplete/AddressForm.js
+++ src/components/AddressAutocomplete/AddressForm.js
@@ -36,7 +36,7 @@ type SelectType = {
 
 type StateType = {
   country: ?SelectType,
-  address: ?any,
+  addrdess: ?any,
   autocompleteValue: ?string,
   predictions: Array<{ mainText: string, secondaryText: string }>,
 };
