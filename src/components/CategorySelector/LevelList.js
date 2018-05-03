0e4a94f147ea321c4598a7578c22a8e6b951d01e
Alexander Korovkin <greenteamer@bk.ru>
2018-05-03 20:09:29 +0300

    refactor getNameText & fix CategorySelector

--- src/components/CategorySelector/LevelList.js
+++ src/components/CategorySelector/LevelList.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import classNames from 'classnames';
 
-import { getNameText } from './utils';
+import { getNameText } from 'utils';
 
 import './CategorySelector.scss';
 
