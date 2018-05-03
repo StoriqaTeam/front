63d1047671b59fd784b2012d5900c8c61095edd9
Levenets Aleksey <a.levenec@storiqa.com>
2018-05-03 17:24:25 +0300

    fix src/utils/rename.js

--- src/utils/rename.js
+++ src/utils/rename.js
@@ -11,20 +11,23 @@ import {
   reduce,
 } from 'ramda';
 
-const rename = curry((fn, obj) =>
+const rename = curry((fn: Function, obj: Object) =>
   pipe(toPairs, map(adjust(fn, 0)), fromPairs)(obj),
 );
 
 export const renameCamelCase = (obj: any) => {
+  // $FlowIgnore
   const reduceIndexed = addIndex(reduce);
-  // const re = /[p{P}p{S}]/;
-  const func = val =>
+  const func = (val: string) =>
     reduceIndexed(
       (acc, next, index) => {
         if (index === 0) return next;
+        // $FlowIgnore
         return acc + next[0].toUpperCase() + next.slice(1).toLowerCase();
       },
+      // $FlowIgnore
       '',
+      // $FlowIgnore
       val.split('_'),
     );
   return rename(func, obj);
