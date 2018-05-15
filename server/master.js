6a2b46517be7851dd4fa3e474f900360657ab720
Saldin Ilya <saldin.iliya@gmail.com>
2018-05-15 13:50:35 +0300

    Create 4 workers

--- server/master.js
+++ server/master.js
@@ -1,14 +1,18 @@
+/* eslint-disable */
 const cluster = require('cluster');
 
+const NUM_OF_WORKERS = 4;
+
 cluster.on('disconnect', worker => {
-  // eslint-disable-next-line
   console.log(`Worker ${worker.id} died`);
   cluster.fork();
 });
 
 cluster.on('online', worker => {
-  // eslint-disable-next-line
   console.log(`Worker ${worker.id} running`);
 });
 
-cluster.fork();
+for (let i = 0; i < NUM_OF_WORKERS; i++) {
+  cluster.fork();
+}
+/* eslint-enable */
