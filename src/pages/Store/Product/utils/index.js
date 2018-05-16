500992b5fca55314616a4f98ce15293f7272b27a
Аневризма <aneurysmjs@outlook.com>
2018-05-16 12:34:01 +0300

    refactor (Product/utils) remove 'buildWidgets' and 'compareWidgets' since they're not necessary anymore

--- src/pages/Store/Product/utils/index.js
+++ src/pages/Store/Product/utils/index.js
@@ -1,5 +1,3 @@
-export { default as buildWidgets } from './buildWidgets';
-export { default as compareWidgets } from './compareWidgets';
 export { default as extractPhotos } from './extractPhotos';
 export { default as extractPriceInfo } from './extractPriceInfo';
 export { default as filterVariants } from './filterVariants';
