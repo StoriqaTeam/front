005f91525a513dfff6edc41d00b23f15a916f9b0
Аневризма <aneurysmjs@outlook.com>
2018-05-22 19:04:13 +0300

    refactor (ProductStore) rewrite as a functional component

--- src/pages/Store/Product/ProductStore.js
+++ src/pages/Store/Product/ProductStore.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React, { Component } from 'react';
+import React from 'react';
 
 import Rating from 'components/Rating';
 
@@ -13,46 +13,41 @@ import { ProductContext } from './index';
 
 import './ProductStore.scss';
 
-class ProductStore extends Component<{}> {
-  handleClick = (): void => {};
-  render() {
-    return (
-      <ProductContext.Consumer>
-        {({ store }: ProductStoreType) => (
-          <div styleName="container">
-            <div styleName="storeInfoWrapper">
-              <div styleName="storeInfo">
-                <div styleName="image" />
-                <div>
-                  <h5 styleName="storeName">{extractText(store.name)}</h5>
-                  <Rating rating={store.rating} />
-                </div>
-              </div>
-              <div styleName="storeDetails">
-                <p>{store.productsCount} goods</p>
-                <p>97,5% user reviews</p>
-              </div>
-            </div>
-            <div styleName="iconsWrapper">
-              <div styleName="iconInfo">
-                <span styleName="icon">
-                  <ChatIcon />
-                </span>
-                <small styleName="iconInfoText">Contact seller</small>
-              </div>
-              <span styleName="separator" />
-              <div styleName="iconInfo">
-                <span styleName="icon">
-                  <HeartIcon />
-                </span>
-                <small styleName="iconInfoText">To Favorites</small>
-              </div>
+const ProductStore = () => (
+  <ProductContext.Consumer>
+    {({ store }: ProductStoreType) => (
+      <div styleName="container">
+        <div styleName="storeInfoWrapper">
+          <div styleName="storeInfo">
+            <div styleName="image" />
+            <div>
+              <h5 styleName="storeName">{extractText(store.name)}</h5>
+              <Rating rating={store.rating} />
             </div>
           </div>
-        )}
-      </ProductContext.Consumer>
-    );
-  }
-}
+          <div styleName="storeDetails">
+            <p>{store.productsCount} goods</p>
+            <p>97,5% user reviews</p>
+          </div>
+        </div>
+        <div styleName="iconsWrapper">
+          <div styleName="iconInfo">
+            <span styleName="icon">
+              <ChatIcon />
+            </span>
+            <small styleName="iconInfoText">Contact seller</small>
+          </div>
+          <span styleName="separator" />
+          <div styleName="iconInfo">
+            <span styleName="icon">
+              <HeartIcon />
+            </span>
+            <small styleName="iconInfoText">To Favorites</small>
+          </div>
+        </div>
+      </div>
+    )}
+  </ProductContext.Consumer>
+);
 
 export default ProductStore;
