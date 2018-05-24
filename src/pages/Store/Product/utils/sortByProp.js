27a9bb74a0edd208e04791dd3e4498067f6e98ee
Аневризма <aneurysmjs@outlook.com>
2018-05-24 17:55:33 +0300

    fix (sortByProp) apply flow typing

--- src/pages/Store/Product/utils/sortByProp.js
+++ src/pages/Store/Product/utils/sortByProp.js
@@ -3,9 +3,11 @@
 import { ascend, prop, sortWith } from 'ramda';
 import type { WidgetType } from '../types';
 
-const sortByProp: string => (
-  Array<WidgetType>,
-  // $FlowIgnoreMe
-) => Array<WidgetType> = propName => sortWith([ascend(prop(propName))]);
+const sortByProp = (propName: string) => (arr: Array<WidgetType>): Array<WidgetType> => {
+  const comparator = ascend(prop(propName));
+  // TODO: Дьжэро, later you must apply proper signature
+  const sort: Function = sortWith([comparator]);
+  return sort(arr);
+};
 
 export default sortByProp;
