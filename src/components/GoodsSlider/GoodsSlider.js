4d7060d85b629863be572412e17c1656475b37a6
Levenets Aleksey <a.levenec@storiqa.com>
2018-06-04 15:56:25 +0300

    blocks in width

--- src/components/GoodsSlider/GoodsSlider.js
+++ src/components/GoodsSlider/GoodsSlider.js
@@ -3,6 +3,7 @@
 import React, { PureComponent } from 'react';
 
 import { Slider } from 'components/Slider';
+// import { Container, Row, Col } from 'layout';
 
 import './GoodsSlider.scss';
 
@@ -40,17 +41,20 @@ class GoodsSlider extends PureComponent<PropsTypes> {
   render() {
     const { items, title, seeAllUrl } = this.props;
     return (
-      <div styleName="container">
-        <Slider
-          infinity
-          animationSpeed={500}
-          title={title}
-          type="products"
-          items={items}
-          slidesToShow={4}
-          seeAllUrl={seeAllUrl}
-        />
-      </div>
+      <Slider
+        infinity
+        animationSpeed={500}
+        title={title}
+        type="products"
+        items={items}
+        slidesToShow={4}
+        seeAllUrl={seeAllUrl}
+        responsive={[
+          { breakpoint: 1344, slidesToShow: 3 },
+          { breakpoint: 992, slidesToShow: 2 },
+          { breakpoint: 576, slidesToShow: 1 },
+        ]}
+      />
     );
   }
 }
