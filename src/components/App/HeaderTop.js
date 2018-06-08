8e4f123c8012f79331d954275e628d8fa5a6499f
levenecav <a.levenec@gmail.com>
2018-06-08 12:30:03 +0300

    remove react responsive (#334)

--- src/components/App/HeaderTop.js
+++ src/components/App/HeaderTop.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import MediaQuery from 'react-responsive';
 
 import { Select } from 'components/common/Select';
-import { LG } from 'utils/responsive';
 
 import './HeaderTop.scss';
 
@@ -10,53 +8,51 @@ class HeaderTop extends Component {
   handleSelect = () => {};
   render() {
     return (
-      <MediaQuery minWidth={LG}>
-        <div styleName="container">
-          <div styleName="item">
-            <Select
-              activeItem={{ id: '1', label: 'BTC' }}
-              items={[
-                { id: '1', label: 'BTC' },
-                { id: '2', label: 'ETH' },
-                { id: '3', label: 'STQ' },
-                { id: '4', label: 'ADA' },
-                { id: '5', label: 'NEM' },
-                { id: '6', label: 'NEO' },
-                { id: '7', label: 'NEM' },
-                { id: '8', label: 'WAX' },
-                { id: '9', label: 'PPT' },
-                { id: '10', label: 'SUB' },
-                { id: '11', label: 'STRAT' },
-                { id: '12', label: 'WTC' },
-                { id: '13', label: 'EOS' },
-                { id: '14', label: 'LTC' },
-                { id: '15', label: 'LSK' },
-                { id: '16', label: 'NXT' },
-              ]}
-              onSelect={this.handleSelect}
-              dataTest="headerСurrenciesSelect"
-            />
-          </div>
-          <div styleName="item">
-            <Select
-              activeItem={{ id: '1', label: 'ENG' }}
-              items={[
-                { id: '1', label: 'ENG' },
-                { id: '2', label: 'CHN' },
-                { id: '3', label: 'RUS' },
-              ]}
-              onSelect={() => {}}
-              dataTest="headerLanguagesSelect"
-            />
-          </div>
-          <div>
-            <a href="_">Help</a> {/* eslint-disable-line */}
-          </div>
-          <div>
-            <a href="/manage/wizard">Sell on Storiqa</a>
-          </div>
+      <div styleName="container">
+        <div styleName="item">
+          <Select
+            activeItem={{ id: '1', label: 'BTC' }}
+            items={[
+              { id: '1', label: 'BTC' },
+              { id: '2', label: 'ETH' },
+              { id: '3', label: 'STQ' },
+              { id: '4', label: 'ADA' },
+              { id: '5', label: 'NEM' },
+              { id: '6', label: 'NEO' },
+              { id: '7', label: 'NEM' },
+              { id: '8', label: 'WAX' },
+              { id: '9', label: 'PPT' },
+              { id: '10', label: 'SUB' },
+              { id: '11', label: 'STRAT' },
+              { id: '12', label: 'WTC' },
+              { id: '13', label: 'EOS' },
+              { id: '14', label: 'LTC' },
+              { id: '15', label: 'LSK' },
+              { id: '16', label: 'NXT' },
+            ]}
+            onSelect={this.handleSelect}
+            dataTest="headerСurrenciesSelect"
+          />
         </div>
-      </MediaQuery>
+        <div styleName="item">
+          <Select
+            activeItem={{ id: '1', label: 'ENG' }}
+            items={[
+              { id: '1', label: 'ENG' },
+              { id: '2', label: 'CHN' },
+              { id: '3', label: 'RUS' },
+            ]}
+            onSelect={() => {}}
+            dataTest="headerLanguagesSelect"
+          />
+        </div>
+        <div>
+          <a href="_">Help</a> {/* eslint-disable-line */}
+        </div>
+        <div>
+          <a href="/manage/wizard">Sell on Storiqa</a>
+        </div>
+      </div>
     );
   }
 }
