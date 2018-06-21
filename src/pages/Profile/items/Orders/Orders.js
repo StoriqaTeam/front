31190ed9f1a22d89cd1f16c49c4e815b9b64ab44
Saldin Ilya <saldin.iliya@gmail.com>
2018-06-21 18:56:10 +0300

    Header

---
+++ src/pages/Profile/items/Orders/Orders.js
@@ -0,0 +1,21 @@
+// @flow
+
+import React, { PureComponent } from 'react';
+
+import Header from './Header';
+
+type PropsType = {
+  //
+};
+
+class Orders extends PureComponent<PropsType> {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default Orders;
