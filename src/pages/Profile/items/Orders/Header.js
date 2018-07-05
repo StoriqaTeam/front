f86d402a9965b25ca76c0a2a00cf7929f4eb9dbe
Аневризма <aneurysmjs@outlook.com>
2018-07-05 19:14:14 +0300

    feat (Orders/Header) apply responsive layout

--- src/pages/Profile/items/Orders/Header.js
+++ src/pages/Profile/items/Orders/Header.js
@@ -2,6 +2,7 @@
 
 import React, { Component } from 'react';
 
+import { Col } from 'layout';
 import { Input } from 'components/common/Input';
 import { Select } from 'components/common/Select';
 import { Button } from 'components/common/Button';
@@ -35,44 +36,52 @@ class Header extends Component<PropsType, StateType> {
     const { searchTerm } = this.state;
     return (
       <div styleName="container">
-        <div styleName="inputsWrapper">
-          <div styleName="searchTermInputWrapper">
-            <Input
-              id="searchTermInput"
-              label="Search order"
-              onChange={this.handleSearchTermChange}
-              value={searchTerm || ''}
-              limit={100}
-              icon="magnifier"
-              fullWidth
+        <Col sm={6} md={6} lg={3} xl={3}>
+          <Input
+            id="searchTermInput"
+            label="Search order"
+            onChange={this.handleSearchTermChange}
+            value={searchTerm || ''}
+            limit={100}
+            icon="magnifier"
+            fullWidth
+          />
+        </Col>
+        <Col sm={1} md={1} lg={3} xl={3}>
+          <div styleName="inputsWrapper">
+            <Select
+              items={[{ id: 'title', label: 'Order status' }]}
+              activeItem={{ id: 'title', label: 'Order status' }}
+              dataTest="OrderStatusSelect"
+              forForm
+              onSelect={this.handleOrderStatusChange}
+              containerStyle={{
+                width: '26.25rem',
+                marginLeft: '3rem',
+                marginBottom: '1px',
+              }}
             />
           </div>
-          <Select
-            items={[{ id: 'title', label: 'Order status' }]}
-            activeItem={{ id: 'title', label: 'Order status' }}
-            dataTest="OrderStatusSelect"
-            forForm
-            onSelect={this.handleOrderStatusChange}
-            containerStyle={{
-              width: '26.25rem',
-              marginLeft: '3rem',
-              marginBottom: '1px',
-            }}
-          />
-          <Select
-            items={[{ id: 'title', label: 'Order date' }]}
-            activeItem={{ id: 'title', label: 'Order date' }}
-            dataTest="OrderDateSelect"
-            forForm
-            onSelect={this.handleOrderDateChange}
-            containerStyle={{ width: '26.25rem', marginLeft: '3rem' }}
-          />
-        </div>
-        <div styleName="buttonWrapper">
-          <Button wireframe big>
-            Open ticket
-          </Button>
-        </div>
+        </Col>
+        <Col sm={1} md={1} lg={3} xl={3}>
+          <div styleName="inputsWrapper">
+            <Select
+              items={[{ id: 'title', label: 'Order date' }]}
+              activeItem={{ id: 'title', label: 'Order date' }}
+              dataTest="OrderDateSelect"
+              forForm
+              onSelect={this.handleOrderDateChange}
+              containerStyle={{ width: '26.25rem', marginLeft: '3rem' }}
+            />
+          </div>
+        </Col>
+        <Col sm={4} md={4} lg={3} xl={3}>
+          <div styleName="buttonWrapper">
+            <Button wireframe big>
+              Open ticket
+            </Button>
+          </div>
+        </Col>
       </div>
     );
   }
