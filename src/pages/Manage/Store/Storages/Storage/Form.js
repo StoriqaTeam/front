97832b72432ee2fcf3dba5a031b863e466c05905
Аневризма <aneurysmjs@outlook.com>
2018-07-19 19:06:50 +0300

    feat (Storage/Form) apply responsiveness

--- src/pages/Manage/Store/Storages/Storage/Form.js
+++ src/pages/Manage/Store/Storages/Storage/Form.js
@@ -86,7 +86,7 @@ class Form extends Component<PropsType, StateType> {
     const { name, addressFull, formErrors } = this.state;
     return (
       <div styleName="form">
-        <div styleName="formItem">
+        <div styleName="formItem storageName">
           <Input
             id="name"
             value={name || ''}
@@ -94,6 +94,7 @@ class Form extends Component<PropsType, StateType> {
             onChange={this.handleInputChange}
             errors={propOr(null, 'name', formErrors)}
             limit={50}
+            fullWidth
           />
         </div>
         <div styleName="formItem">
