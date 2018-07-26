19d7c1ed011878c4f98d51d3eeb8aa8f9bbed213
Alexander Korovkin <teamer777@gmail.com>
2018-07-26 16:49:49 +0300

    fix padding (#437)
    * fix padding
    
    * remove commits

--- src/pages/Manage/Store/ManageStoreDecorator.js
+++ src/pages/Manage/Store/ManageStoreDecorator.js
@@ -65,26 +65,28 @@ export default (OriginalComponent: any, title: string, isNewStore?: boolean) =>
       return (
         <AppContext.Consumer>
           {({ environment }) => (
-            <Container>
-              <Row>
-                <Col sm={3} md={3} lg={2} xl={2}>
-                  <Menu {...menuProps} environment={environment} />
-                </Col>
-                <Col sm={9} md={9} lg={10} xl={10}>
-                  <div styleName="container">
-                    <div styleName="header">
-                      <span styleName="title">{title}</span>
+            <div styleName="wrapper">
+              <Container>
+                <Row>
+                  <Col sm={3} md={3} lg={2} xl={2}>
+                    <Menu {...menuProps} environment={environment} />
+                  </Col>
+                  <Col sm={9} md={9} lg={10} xl={10}>
+                    <div styleName="container">
+                      <div styleName="header">
+                        <span styleName="title">{title}</span>
+                      </div>
+                      <div styleName="form">
+                        <OriginalComponent
+                          environment={environment}
+                          {...formProps}
+                        />
+                      </div>
                     </div>
-                    <div styleName="form">
-                      <OriginalComponent
-                        environment={environment}
-                        {...formProps}
-                      />
-                    </div>
-                  </div>
-                </Col>
-              </Row>
-            </Container>
+                  </Col>
+                </Row>
+              </Container>
+            </div>
           )}
         </AppContext.Consumer>
       );
