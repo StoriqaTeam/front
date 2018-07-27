232c6c6d086a6fceadb4346e85fec91ab5c116bb
levenecav <a.levenec@gmail.com>
2018-07-27 11:41:28 +0300

    Leve/trello/468 search stores products (#438)
    * add search for store
    
    * add serach stores products
    
    * edits for PR

---
+++ src/components/SearchNoResults/SearchNoResults.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { Icon } from 'components/Icon';
+
+import './SearchNoResults.scss';
+
+type PropsType = {
+  value: ?string,
+};
+
+const SearchNoResults = (props: PropsType) => (
+  <div styleName="container">
+    <Icon type="searchNoResults" size={120} />
+    <div
+      styleName="text" /* eslint-disable */
+      dangerouslySetInnerHTML={{
+        /* eslint-enable */
+        __html: `Sorry, but no results${
+          props.value
+            ? ` for ‘<strong>${
+                props.value
+              }</strong>’. Check your search request for mistakes or try to find somethind else.`
+            : ''
+        }.`,
+      }}
+    />
+  </div>
+);
+
+export default SearchNoResults;
