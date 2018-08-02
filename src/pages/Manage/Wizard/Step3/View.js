bd156d715f36fcca6bc7bf4480379fbe41943b7e
Alexander Korovkin <teamer777@gmail.com>
2018-08-02 12:53:12 +0300

    Aleks/trello/499 adaptive wizard (#453)
    * footer adaptive
    
    * stepper adaptive
    
    * fix padding footer
    
    * adaptive for third step
    
    * work with product
    
    * fix additional photos adaptive
    
    * wizard adaptive middle result
    
    * uploaders
    
    * attributes
    
    * fix ci
    
    * fix warnings
    
    * add delete product modal
    
    * fix ci

--- src/pages/Manage/Wizard/Step3/View.js
+++ src/pages/Manage/Wizard/Step3/View.js
@@ -3,15 +3,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { omit, pathOr, head, map, addIndex, isEmpty, filter } from 'ramda';
+import { Col, Row } from 'layout';
 
 import { Icon } from 'components/Icon';
 import { CardProduct } from 'components/CardProduct';
 import { Button } from 'components/common/Button';
 import { getNameText } from 'utils';
+import { Modal } from 'components/Modal';
+
+import ProductLayer from './ProductLayer';
+import FormWrapper from '../FormWrapper';
 
 import type { BaseProductNodeType } from '../Wizard';
 import Form from './Form';
-import Modal from './Modal';
 
 import './View.scss';
 
@@ -51,15 +55,18 @@ type PropsType = {
   onSave: () => void,
   onClearProductState: () => void,
   formStateData: BaseProductNodeType,
+  onChangeEditingProduct: (val: boolean) => void,
 };
 
 type StateType = {
   showForm: boolean,
+  deleteId: ?string,
 };
 
 class ThirdStepView extends React.Component<PropsType, StateType> {
   state = {
     showForm: false,
+    deleteId: null,
   };
 
   prepareAttributesValues = (
@@ -79,7 +86,7 @@ class ThirdStepView extends React.Component<PropsType, StateType> {
     );
 
   handleOnShowForm = (item: ProductNodeType) => {
-    const { onChange, formStateData } = this.props;
+    const { onChange, formStateData, onChangeEditingProduct } = this.props;
     const name = item.name ? getNameText(item.name, 'EN') : '';
     const shortDescription = item.shortDescription
       ? getNameText(item.shortDescription, 'EN')
@@ -103,25 +110,89 @@ class ThirdStepView extends React.Component<PropsType, StateType> {
     };
     return () => {
       onChange(prepareStateObj);
-      this.setState({ showForm: true });
+      this.setState({ showForm: true }, () => onChangeEditingProduct(true));
     };
   };
 
   handleOnCloseModal = () => {
-    const { onClearProductState } = this.props;
+    const { onClearProductState, onChangeEditingProduct } = this.props;
     this.setState({ showForm: false }, () => {
       onClearProductState();
+      onChangeEditingProduct(false);
     });
   };
 
-  handleOnDelete = (ID: string) => () => {
+  handleOnDelete = (ID: ?string) => () => {
     const { onDelete } = this.props;
-    onDelete(ID);
+    if (ID) {
+      onDelete(ID);
+      this.handleOnCloseDelete();
+    }
+  };
+
+  handleOnCloseDelete = () => {
+    this.setState({ deleteId: null });
+  };
+
+  handleOnShowDelete = (ID: string) => () => {
+    this.setState({ deleteId: ID });
+  };
+
+  renderUploaderItem = () => {
+    const { onChangeEditingProduct } = this.props;
+    return (
+      <div
+        styleName="productItem uploaderItem"
+        role="button"
+        onClick={() =>
+          this.setState({ showForm: true }, () => onChangeEditingProduct(true))
+        }
+        onKeyDown={() => {}}
+        tabIndex={0}
+        data-test="wizardUploaderProductFoto"
+      >
+        <div styleName="productContent">
+          <Icon type="cameraPlus" size={56} />
+          <span styleName="buttonLabel">Add new product</span>
+        </div>
+      </div>
+    );
+  };
+
+  renderFirstUploaderItem = () => {
+    const { onChangeEditingProduct } = this.props;
+    return (
+      <div styleName="firstUploaderItem">
+        <div styleName="firstUploaderItemWrapper">
+          <div styleName="icon">
+            <Icon type="cameraPlus" size={80} />
+          </div>
+          <div styleName="text">
+            Currently you have no products in your store. Click ‘Add’ to start
+            filling your store with products.
+          </div>
+          <div styleName="button">
+            <Button
+              onClick={() =>
+                this.setState({ showForm: true }, () =>
+                  onChangeEditingProduct(true),
+                )
+              }
+              dataTest="wizardUploaderProductFotoFirst"
+              big
+              wireframe
+            >
+              <span>Add first product</span>
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
   };
 
   render() {
     const { formStateData, onChange, products, onUpload, onSave } = this.props;
-    const { showForm } = this.state;
+    const { showForm, deleteId } = this.state;
     const productsArr = map(item => item.node, products);
     const filteredProductsArr = filter(item => {
       // $FlowIgnoreMe
@@ -129,89 +200,85 @@ class ThirdStepView extends React.Component<PropsType, StateType> {
       return !isEmpty(edges);
     }, productsArr);
     const mapIndexed = addIndex(map);
+    if (showForm) {
+      return (
+        <Form
+          data={formStateData}
+          categories={this.context.directories.categories}
+          onChange={onChange}
+          onUpload={onUpload}
+          onSave={onSave}
+          onClose={this.handleOnCloseModal}
+        />
+      );
+    }
     return (
-      <div styleName="view">
-        {!isEmpty(filteredProductsArr) ? (
-          <div
-            styleName="productItem uploaderItem"
-            role="button"
-            onClick={() => this.setState({ showForm: true })}
-            onKeyDown={() => {}}
-            tabIndex={0}
-            data-test="wizardUploaderProductFoto"
-          >
-            <div styleName="productContent">
-              <Icon type="cameraPlus" size={56} />
-              <span styleName="buttonLabel">Add new product</span>
-            </div>
-          </div>
-        ) : (
-          <div styleName="firstUploaderItem">
-            <div styleName="firstUploaderItemWrapper">
-              <div styleName="icon">
-                <Icon type="cameraPlus" size={80} />
-              </div>
-              <div styleName="text">
-                Currently you have no products in your store. Click ‘Add’ to
-                start filling your store with products.
-              </div>
-              <div styleName="button">
-                <Button
-                  onClick={() => this.setState({ showForm: true })}
-                  dataTest="wizardUploaderProductFotoFirst"
-                  big
-                  wireframe
-                >
-                  <span>Add first product</span>
-                </Button>
-              </div>
-            </div>
-          </div>
-        )}
-        {!isEmpty(filteredProductsArr) &&
-          mapIndexed(
-            (item, index) => (
-              <div key={index} styleName="productItem cardItem">
-                <div styleName="productContent">
-                  <CardProduct item={item} />
-                  <div styleName="layer">
-                    <div
-                      styleName="editbutton"
-                      onClick={this.handleOnShowForm(item)}
-                      role="button"
-                      onKeyDown={() => {}}
-                      tabIndex={0}
-                    >
-                      <Icon type="note" size={56} />
-                      <span styleName="buttonLabel">Edit product</span>
+      <FormWrapper
+        thirdForm
+        title="Fill your store with goods"
+        description="Choose what you gonna sale in your marketplace and add it with ease"
+      >
+        <div styleName="view">
+          <Row>
+            {!isEmpty(filteredProductsArr) ? (
+              <Col size={12} md={4} xl={3}>
+                {this.renderUploaderItem()}
+              </Col>
+            ) : (
+              this.renderFirstUploaderItem()
+            )}
+            {!isEmpty(filteredProductsArr) &&
+              mapIndexed(
+                (item, index) => (
+                  <Col size={12} md={4} xl={3} key={index}>
+                    <div styleName="productItem cardItem">
+                      <div styleName="productContent">
+                        <CardProduct item={item} />
+                        <ProductLayer
+                          // onDelete={this.handleOnDelete(item.id)}
+                          onDelete={this.handleOnShowDelete(item.id)}
+                          onEdit={this.handleOnShowForm(item)}
+                        />
+                      </div>
                     </div>
-                    <div
-                      styleName="editbutton"
-                      onClick={this.handleOnDelete(item.id)}
-                      role="button"
-                      onKeyDown={() => {}}
-                      tabIndex={0}
+                  </Col>
+                ),
+                filteredProductsArr,
+              )}
+          </Row>
+          <Modal showModal={deleteId} onClose={this.handleOnCloseDelete}>
+            <div styleName="deleteWrapper">
+              <div styleName="deleteContent">
+                <div styleName="title">Delete your product?</div>
+                <div styleName="description">
+                  Are you sure you want to delete this listing? All the listing
+                  information will be discarded and cannot be retrieved.
+                </div>
+                <div styleName="buttonsContainer">
+                  <Button
+                    onClick={this.handleOnCloseDelete}
+                    dataTest="wizardBackButton"
+                    wireframe
+                    big
+                  >
+                    <span>Cancel</span>
+                  </Button>
+                  <div styleName="deleteButton">
+                    <Button
+                      onClick={this.handleOnDelete(deleteId)}
+                      dataTest="wizardBackButton"
+                      big
+                      pink
                     >
-                      <Icon type="basket" size={56} />
-                      <span styleName="buttonLabel">Delete product</span>
-                    </div>
+                      <span>Yes, delete, please</span>
+                    </Button>
                   </div>
                 </div>
               </div>
-            ),
-            filteredProductsArr,
-          )}
-        <Modal showModal={showForm} onClose={this.handleOnCloseModal}>
-          <Form
-            data={formStateData}
-            categories={this.context.directories.categories}
-            onChange={onChange}
-            onUpload={onUpload}
-            onSave={onSave}
-            onClose={this.handleOnCloseModal}
-          />
-        </Modal>
-      </div>
+            </div>
+          </Modal>
+        </div>
+      </FormWrapper>
     );
   }
 }
