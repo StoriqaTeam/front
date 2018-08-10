b0364241550051eabdc177d090a0c91f33941eed
levenecav <a.levenec@gmail.com>
2018-08-10 11:11:36 +0300

    Leve/trello/526 manage variant (#477)
    * create new product with variant and etc
    
    * added photo delete functionality

--- src/pages/Manage/Store/Products/Product/Variants/Table/Table.js
+++ src/pages/Manage/Store/Products/Product/Variants/Table/Table.js
@@ -1,9 +1,12 @@
 // @flow
 
-import React, { Component } from 'react';
-import { map, propEq } from 'ramda';
+import React, { Component, Fragment } from 'react';
+import { isEmpty, map, propEq, length } from 'ramda';
+import classNames from 'classnames';
 
 import { log } from 'utils';
+
+import { ProductFormContext } from 'pages/Manage/Store/Products/Product';
 
 import Row from './Row';
 import Form from './Form/Form';
@@ -13,11 +16,40 @@ import './Table.scss';
 
 type PropsType = {
   category: {},
-  variants: Array<{ rawId: number }>,
+  variants: Array<{
+    id: string,
+    rawId: number,
+    vendorCode: string,
+    price: number,
+    cashback: number,
+    discount: number,
+    attributes: Array<{
+      attribute: {
+        name: Array<{ text: string }>,
+      },
+      value: string,
+    }>,
+    stocks: Array<{
+      id: string,
+      productId: number,
+      warehouseId: string,
+      quantity: number,
+      warehouse: {
+        name: string,
+        addressFull: {
+          value: string,
+        },
+      },
+    }>,
+  }>,
   productRawId: number,
   productId: string,
   storeID: string,
   handleDeleteVariant: (id: string) => void,
+  comeResponse: boolean,
+  resetComeResponse: () => void,
+  isNewVariant: boolean,
+  toggleNewVariantParam: (value: boolean) => void,
 };
 
 type StateType = {
@@ -27,6 +59,25 @@ type StateType = {
 class Table extends Component<PropsType, StateType> {
   state: StateType = {
     expandedItemId: null,
+  };
+
+  componentDidUpdate(prevProps: PropsType) {
+    const { comeResponse, isNewVariant } = this.props;
+    if (comeResponse && comeResponse !== prevProps.comeResponse) {
+      this.resetComeResponse();
+    }
+    if (isNewVariant && isNewVariant !== prevProps.isNewVariant) {
+      this.resetExpandedItemId();
+    }
+  }
+
+  resetComeResponse = () => {
+    const { resetComeResponse } = this.props;
+    this.setState({ expandedItemId: null }, resetComeResponse);
+  };
+
+  resetExpandedItemId = () => {
+    this.setState({ expandedItemId: null });
   };
 
   handleCreateVariant = (variant: {}) => {
@@ -46,6 +97,7 @@ class Table extends Component<PropsType, StateType> {
       this.setState({ expandedItemId: null });
     } else {
       this.setState({ expandedItemId: id });
+      this.props.toggleNewVariantParam(false);
     }
   };
 
@@ -53,55 +105,85 @@ class Table extends Component<PropsType, StateType> {
     this.props.handleDeleteVariant(id);
   };
 
-  renderRows = () => {
+  renderRows = (
+    handleSaveBaseProductWithVariant: () => void,
+    isLoading: boolean,
+  ) => {
     const { expandedItemId } = this.state;
-    return map(item => {
-      if (propEq('rawId', expandedItemId, item)) {
-        return (
-          <Form
-            category={this.props.category}
+    return map(
+      item => (
+        <Fragment key={item.id}>
+          <Row
             variant={item}
-            productRawId={this.props.productRawId}
-            productId={this.props.productId}
-            key={item.id}
-            isExpanded
             onExpandClick={this.expandRow}
-            storeID={this.props.storeID}
-            handleCollapseVariant={this.handleCollapseVariant}
+            handleDeleteVariant={this.handleDeleteVariant}
+            isOpen={item.rawId === expandedItemId}
+            notRemove={length(this.props.variants) === 1}
           />
-        );
-      }
-      return (
-        <Row
-          key={item.id}
-          variant={item}
-          onExpandClick={this.expandRow}
-          handleDeleteVariant={this.handleDeleteVariant}
-        />
-      );
-    }, this.props.variants);
+          {propEq('rawId', expandedItemId, item) && (
+            <Form
+              isLoading={isLoading}
+              category={this.props.category}
+              variant={item}
+              productRawId={this.props.productRawId}
+              productId={this.props.productId}
+              key={item.id}
+              isExpanded
+              onExpandClick={this.expandRow}
+              storeID={this.props.storeID}
+              handleCollapseVariant={this.handleCollapseVariant}
+              handleSaveBaseProductWithVariant={
+                handleSaveBaseProductWithVariant
+              }
+            />
+          )}
+        </Fragment>
+      ),
+      this.props.variants,
+    );
   };
 
   render() {
+    const { variants, isNewVariant, toggleNewVariantParam } = this.props;
+    const { expandedItemId } = this.state;
     return (
-      <div>
-        <div style={{ overflow: 'hidden' }}>
-          <div styleName="table">
-            <div styleName="tableWrapper">
-              <Header onSelectAllClick={this.handleSelectAll} />
-              {this.renderRows()}
-            </div>
+      <ProductFormContext.Consumer>
+        {({ isLoading, handleSaveBaseProductWithVariant }) => (
+          <div
+            styleName={classNames('container', {
+              hiddenButton: expandedItemId || isNewVariant,
+              newVariant: isEmpty(variants),
+            })}
+          >
+            {!isEmpty(variants) && (
+              <div styleName="table">
+                <Header
+                  onSelectAllClick={this.handleSelectAll}
+                  notRemove={length(this.props.variants) === 1}
+                />
+                {this.renderRows(handleSaveBaseProductWithVariant, isLoading)}
+              </div>
+            )}
+            {(isEmpty(variants) || isNewVariant) &&
+              !expandedItemId && (
+                <div styleName="emptyForm">
+                  <Form
+                    isLoading={isLoading}
+                    category={this.props.category}
+                    productRawId={this.props.productRawId}
+                    productId={this.props.productId}
+                    storeID={this.props.storeID}
+                    handleSaveBaseProductWithVariant={
+                      handleSaveBaseProductWithVariant
+                    }
+                    isNewVariant={isNewVariant}
+                    toggleNewVariantParam={toggleNewVariantParam}
+                  />
+                </div>
+              )}
           </div>
-        </div>
-        {!this.state.expandedItemId && (
-          <Form
-            category={this.props.category}
-            productRawId={this.props.productRawId}
-            productId={this.props.productId}
-            storeID={this.props.storeID}
-          />
         )}
-      </div>
+      </ProductFormContext.Consumer>
     );
   }
 }
