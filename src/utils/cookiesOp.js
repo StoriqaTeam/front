e7bbee9a81afa5a3389b4a9fe442e6e15566dd97
Saldin Ilya <saldin.iliya@gmail.com>
2018-08-29 12:35:03 +0300

    Fix redirect after login via oauth

--- src/utils/cookiesOp.js
+++ src/utils/cookiesOp.js
@@ -2,18 +2,22 @@
 
 import Cookies from 'universal-cookie';
 
-const cookies = new Cookies();
-
 export const setCookie = (name: string, value: any, expires: any) => {
   let options = { path: '/' };
   if (expires) {
     options = { ...options, expires };
   }
+
+  const cookies = new Cookies();
   cookies.set(name, value, { ...options });
 };
 
 export const removeCookie = (name: string) => {
+  const cookies = new Cookies();
   cookies.remove(name, { path: '/' });
 };
 
-export const getCookie = (name: string) => cookies.get(name);
+export const getCookie = (name: string) => {
+  const cookies = new Cookies();
+  return cookies.get(name);
+};
