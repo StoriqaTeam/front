abdc7b0975aebc96a746c086564dc7c7b8e7a5f1
Levenets Aleksey <a.levenec@storiqa.com>
2018-10-12 16:13:29 +0300

    fix carrot bug

--- server/app.js
+++ server/app.js
@@ -286,7 +286,7 @@ app.use(
                 if ('undefined' == typeof carrotquest) {
                   var e = document.createElement('script');
                   e.type = "text/javascript", e.async = !0, e.src = '//cdn.carrotquest.io/api.min.js', document.getElementsByTagName('head')[0].appendChild(e), window.carrotquest = {}, window.carrotquestasync = [], carrotquest.settings = {};
-                  for (var n = ['connect', 'track', 'identify', auth, 'oth', 'onReady', 'addCallback', 'removeCallback', 'trackMessageInteraction'], a = 0; a < n.length; a++) carrotquest[n[a]] = t(n[a])
+                  for (var n = ['connect', 'track', 'identify', 'auth', 'oth', 'onReady', 'addCallback', 'removeCallback', 'trackMessageInteraction'], a = 0; a < n.length; a++) carrotquest[n[a]] = t(n[a])
                 }
               }(), carrotquest.connect('${process.env.REACT_APP_CARROT_QUEST_API_KEY}');
             </script>`
