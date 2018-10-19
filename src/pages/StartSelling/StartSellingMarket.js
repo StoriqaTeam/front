c320c6cb13ac2cb62d252133f22a47656f045342
Levenets Aleksey <a.levenec@storiqa.com>
2018-10-19 13:25:47 +0300

    add new english text to start selling

--- src/pages/StartSelling/StartSellingMarket.js
+++ src/pages/StartSelling/StartSellingMarket.js
@@ -22,7 +22,7 @@ const StartSellingMarket = ({ lang }: PropsType) => (
         __html:
           lang === 'ru'
             ? 'Storiqa дает возможность продавать ваши товары в&nbsp;Сингапуре уже сейчас'
-            : 'Storiqa give you the opportunity to&nbsp;sell your goods in&nbsp;Singapore today',
+            : 'Storiqa gives you an&nbsp;opportunity to&nbsp;sell all around the&nbsp;globe. Launching with select countries now!',
       }}
     />
     <p
@@ -33,7 +33,7 @@ const StartSellingMarket = ({ lang }: PropsType) => (
         __html:
           lang === 'ru'
             ? 'Наша цель&nbsp;— предоставить высококлассный сервис продавцу и&nbsp;гарантировать наивысшее качество товаров покупателю.'
-            : 'Our goal is to guarantee the&nbsp;quality of&nbsp;experience and goods, for both buyers and&nbsp;sellers.',
+            : 'We guarantee quality experience for both buyers and&nbsp;sellers.',
       }}
     />
     <Row>
@@ -46,7 +46,7 @@ const StartSellingMarket = ({ lang }: PropsType) => (
               __html:
                 lang === 'ru'
                   ? 'Storiqa&nbsp;— это онлайн-маркетплейс, который предлагает производителям из&nbsp;России, Сингапура, Малайзии, Индонезии и&nbsp;Тайланда создать магазин на&nbsp;платформе и&nbsp;продавать свои товары в&nbsp;странах APAC: на&nbsp;первом этапе&nbsp;— в&nbsp;Сингапуре, и&nbsp;количество доступных стран будет увеличиваться.'
-                  : 'Storiqa is an&nbsp;marketplace which offer sellers from Singapore, Thailand, Malasia, Indonesia and Russia create their shop on&nbsp;Storiqa now and start selling their goods to the APAC countries. For the first step Storiqa will support Singapore only and every month we will add more and more contries.',
+                  : 'Currently accepting sellers from Singapore and&nbsp;Russia. Up&nbsp;next Malaysia, Indonesia, and&nbsp;Thailand.',
             }}
           />
           <p
@@ -56,7 +56,7 @@ const StartSellingMarket = ({ lang }: PropsType) => (
               __html:
                 lang === 'ru'
                   ? 'Мы ищем производителей товаров ручной работы или мелкосерийные производства. Если вы&nbsp;создаете красивые, уникальные и&nbsp;вдохновляющие вещи&nbsp;— найдите новую аудиторию вместе с&nbsp;нами!'
-                  : 'We are inviting small-scale producers of&nbsp;craft and tailor made goods. If it is you and you are creating unique, good quality and wonderful goods&nbsp;— apply our program today.',
+                  : 'We are inviting small-scale producers of&nbsp;craft and tailor made goods. If you are creating unique, wonderfully made, great quality goods&nbsp;— apply today.',
             }}
           />
         </article>
