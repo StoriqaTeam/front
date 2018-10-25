1fe353c14dd5c67a8e826f8a11737b71c8c04f20
Аневризма <aneurysmjs@outlook.com>
2018-10-25 11:34:35 +0300

    feat (Manage/Store/InvoiceTotal) add i18n support

--- src/pages/Manage/Store/OrderInvoice/InvoiceTotal/index.js
+++ src/pages/Manage/Store/OrderInvoice/InvoiceTotal/index.js
@@ -1,7 +1,9 @@
 // @flow strict
 import React, { Fragment } from 'react';
 
-import './OrderInvoice.scss';
+import '../OrderInvoice.scss';
+
+import t from './i18n';
 
 type PropsType = {
   total: string,
@@ -14,14 +16,14 @@ const InvoiceTotal = ({ total, shipping }: PropsType) => (
       <div styleName="totalFieldItem" />
       <div styleName="totalFieldItem" />
       <div styleName="totalFieldItem" />
-      <div styleName="totalFieldItem shipping">SHIPPING & HANDLING</div>
+      <div styleName="totalFieldItem shipping">{t.shippingAndHandling}</div>
       <div styleName="totalFieldItem shipping">{shipping}</div>
     </div>
     <div styleName="totalField">
       <div styleName="totalFieldItem" />
       <div styleName="totalFieldItem" />
       <div styleName="totalFieldItem" />
-      <div styleName="totalFieldItem total">TOTAL</div>
+      <div styleName="totalFieldItem total">{t.total}</div>
       <div styleName="totalFieldItem total">{total}</div>
     </div>
   </Fragment>
