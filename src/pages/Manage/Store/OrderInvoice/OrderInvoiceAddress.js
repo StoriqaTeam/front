c7996000d76129bf215cb7410fc18d6793f069f9
Аневризма <aneurysmjs@outlook.com>
2018-10-29 17:33:27 +0300

    refactor (OrderInvoiceAddress) format region

--- src/pages/Manage/Store/OrderInvoice/OrderInvoiceAddress.js
+++ src/pages/Manage/Store/OrderInvoice/OrderInvoiceAddress.js
@@ -2,6 +2,8 @@
 
 import React from 'react';
 
+import { setRegion } from './utils';
+
 import './OrderInvoice.scss';
 
 type PropsType = $Shape<{
@@ -23,42 +25,46 @@ const OrderInvoiceData = ({
   value,
   country,
   administrativeAreaLevel1,
+  administrativeAreaLevel2,
   locality,
   political,
   postalCode,
   streetNumber,
   email,
   phone,
-}: PropsType) => (
-  <div styleName="address">
-    <div styleName="addressField">
-      <span>Postal Code:</span> <span>{postalCode}</span>
+}: PropsType) => {
+  const region = setRegion(administrativeAreaLevel1, administrativeAreaLevel2)
+  return (
+    <div styleName="address">
+      <div styleName="addressField">
+        <span>Postal Code:</span> <span>{postalCode}</span>
+      </div>
+      <div styleName="addressField">
+        <span>Country #:</span> <span>{country}</span>
+      </div>
+      <div styleName="addressField">
+        <span>Region:</span> <span> {administrativeAreaLevel1}</span>
+      </div>
+      <div styleName="addressField">
+        <span>City:</span> <span>{locality}</span>
+      </div>
+      <div styleName="addressField">
+        <span>Area/District:</span> <span> {political}</span>
+      </div>
+      <div styleName="addressField">
+        <span>Street:</span> <span> {value}</span>
+      </div>
+      <div styleName="addressField">
+        <span>Apt/Suite/Other:</span> <span> {streetNumber}</span>
+      </div>
+      <div styleName="addressField">
+        <span>Email:</span> <span> {email}</span>
+      </div>
+      <div styleName="addressField">
+        <span>Phone Number:</span> <span> {phone}</span>
+      </div>
     </div>
-    <div styleName="addressField">
-      <span>Country #:</span> <span>{country}</span>
-    </div>
-    <div styleName="addressField">
-      <span>Region:</span> <span> {administrativeAreaLevel1}</span>
-    </div>
-    <div styleName="addressField">
-      <span>Locality:</span> <span>{locality}</span>
-    </div>
-    <div styleName="addressField">
-      <span>Area/District:</span> <span> {political}</span>
-    </div>
-    <div styleName="addressField">
-      <span>Street:</span> <span> {value}</span>
-    </div>
-    <div styleName="addressField">
-      <span>Apt/Suite/Other:</span> <span> {streetNumber}</span>
-    </div>
-    <div styleName="addressField">
-      <span>Email:</span> <span> {email}</span>
-    </div>
-    <div styleName="addressField">
-      <span>Phone Number:</span> <span> {phone}</span>
-    </div>
-  </div>
-);
+  );
+}
 
 export default OrderInvoiceData;
