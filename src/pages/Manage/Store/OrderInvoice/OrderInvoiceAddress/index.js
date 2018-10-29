dd47a5148b6416442ce0d51dd0fb57327f02a857
Аневризма <aneurysmjs@outlook.com>
2018-10-29 17:33:27 +0300

    refactor (OrderInvoiceAddress) format region

--- src/pages/Manage/Store/OrderInvoice/OrderInvoiceAddress/index.js
+++ src/pages/Manage/Store/OrderInvoice/OrderInvoiceAddress/index.js
@@ -2,6 +2,8 @@
 
 import React from 'react';
 
+import { setRegion } from './utils';
+
 import '../OrderInvoice.scss';
 
 import t from './i18n';
@@ -25,42 +27,46 @@ const OrderInvoiceData = ({
   value,
   country,
   administrativeAreaLevel1,
+  administrativeAreaLevel2,
   locality,
   political,
   postalCode,
   streetNumber,
   email,
   phone,
-}: PropsType) => (
-  <div styleName="address">
-    <div styleName="addressField">
-      <span>{t.postalCode}:</span> <span>{postalCode}</span>
+}: PropsType) => {
+  const region = setRegion(administrativeAreaLevel1, administrativeAreaLevel2)
+  return (
+    <div styleName="address">
+      <div styleName="addressField">
+        <span>{t.postalCode}:</span> <span>{postalCode}</span>
+      </div>
+      <div styleName="addressField">
+        <span>{t.country}:</span> <span>{country}</span>
+      </div>
+      <div styleName="addressField">
+        <span>{t.region}:</span> <span> {region}</span>
+      </div>
+      <div styleName="addressField">
+        <span>{t.city}:</span> <span>{locality}</span>
+      </div>
+      <div styleName="addressField">
+        <span>{t.areaDistrict}:</span> <span> {political}</span>
+      </div>
+      <div styleName="addressField">
+        <span>{t.street}:</span> <span> {value}</span>
+      </div>
+      <div styleName="addressField">
+        <span>{t.aptSuiteOther}:</span> <span> {streetNumber}</span>
+      </div>
+      <div styleName="addressField">
+        <span>{t.email}:</span> <span> {email}</span>
+      </div>
+      <div styleName="addressField">
+        <span>{t.phoneNumber}:</span> <span> {phone}</span>
+      </div>
     </div>
-    <div styleName="addressField">
-      <span>{t.country}:</span> <span>{country}</span>
-    </div>
-    <div styleName="addressField">
-      <span>{t.region}:</span> <span> {administrativeAreaLevel1}</span>
-    </div>
-    <div styleName="addressField">
-      <span>{t.locality}:</span> <span>{locality}</span>
-    </div>
-    <div styleName="addressField">
-      <span>{t.areaDistrict}:</span> <span> {political}</span>
-    </div>
-    <div styleName="addressField">
-      <span>{t.street}:</span> <span> {value}</span>
-    </div>
-    <div styleName="addressField">
-      <span>{t.aptSuiteOther}:</span> <span> {streetNumber}</span>
-    </div>
-    <div styleName="addressField">
-      <span>{t.email}:</span> <span> {email}</span>
-    </div>
-    <div styleName="addressField">
-      <span>{t.phoneNumber}:</span> <span> {phone}</span>
-    </div>
-  </div>
-);
+  );
+};
 
 export default OrderInvoiceData;
---
+++ src/pages/Manage/Store/OrderInvoice/OrderInvoiceAddress/utils/index.js
@@ -0,0 +1,17 @@
+// @flow strict  
+
+import { isNil } from 'ramda';
+
+export const setRegion = (admin1: ?string, admin2: ?string): string => {
+  let administrativeAreaLevel = '';
+  if (!isNil(admin1) && !isNil(admin2)) {
+    administrativeAreaLevel =  `${admin1}, ${admin2}`;
+  }
+  if (!isNil(admin1) && isNil(admin2)) {
+    administrativeAreaLevel = admin1;
+  }
+  if (isNil(admin1) && !isNil(admin2)) {
+    administrativeAreaLevel =  admin2;
+  }
+  return administrativeAreaLevel;
+};
