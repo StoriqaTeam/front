556b67dddb33a74d51b8410ba84f1271e04af97b
Аневризма <aneurysmjs@outlook.com>
2018-10-30 17:32:08 +0300

    feat (OrderInvoice) add 'pluckFromOrder' utility function

--- src/pages/Manage/Store/OrderInvoice/OrderInvoice.js
+++ src/pages/Manage/Store/OrderInvoice/OrderInvoice.js
@@ -16,7 +16,7 @@ import {
   InvoiceTotal,
 } from './index';
 
-import { formatStatus } from './utils';
+import { /* formatStatus, */ pluckFromOrder } from './utils';
 
 import './OrderInvoice.scss';
 
@@ -27,15 +27,13 @@ type PropsType = {
 class OrderInvoice extends PureComponent<PropsType> {
   render() {
     const {
-      me: { email, order },
+      me: { order },
     } = this.props;
-    const clonedOrder = !isNil(order) ? order : {};
-    const invoiceData = {
-      receiverName: !isNil(order) ? order.receiverName : '',
-      slug: !isNil(order) ? `${order.slug}` : '',
-      trackId: !isNil(order) ? order.trackId : '',
-      state: !isNil(order) ? formatStatus(order.state) : '',
-    };
+    const fromOrder = pluckFromOrder(order);
+    const clonedOrder = !isNil(order) ? { ...order } : {};
+    const invoiceData = fromOrder(['receiverName', 'slug', 'trackId', 'state']);
+    const customer = !isNil(order) ? order.customer : {};
+    const email = !isNil(customer) ? customer.email : '';
     const address = !isNil(order) ? order.addressFull : {};
     const phone = !isNil(order) ? order.receiverPhone : '';
     const currency = !isNil(order) ? order.currency : '';
@@ -54,6 +52,7 @@ class OrderInvoice extends PureComponent<PropsType> {
           </div>
         </header>
         <div styleName="invoiceDetails">
+          {/* $FlowIgnoreMe */}
           <OrderInvoiceData {...invoiceData} />
           <OrderInvoiceAddress {...invoiceAddress} />
         </div>
--- src/pages/Manage/Store/OrderInvoice/utils/index.js
+++ src/pages/Manage/Store/OrderInvoice/utils/index.js
@@ -1,8 +1,24 @@
 // @flow strict
-import { isNil } from 'ramda';
+
+import { isNil, pick, isEmpty } from 'ramda';
 
 export const formatStatus = (str: string): string =>
   str.replace(/[\W_]+/g, ' ').toLowerCase();
+
+type OrderType = ?{
+  +receiverName: string,
+  +slug: number,
+  +trackId: ?string,
+  +state: string,
+};
+
+export const pluckFromOrder = (odr: OrderType) => (props: Array<string>) => {
+  const order = !isNil(odr) ? { ...odr } : {};
+  if (isEmpty(order)) {
+    return order;
+  }
+  return pick(props, order);
+};
 
 export const setRegion = (admin1: ?string, admin2: ?string): string => {
   let administrativeAreaLevel = '';
