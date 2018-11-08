a8e71909808ed9a61f6d41978855431ae5b49c7d
Saldin Ilya <saldin.iliya@gmail.com>
2018-11-08 18:59:23 +0300

    Fix timestamps for logger.

--- src/utils/log.js
+++ src/utils/log.js
@@ -1,23 +1,28 @@
 // @flow
 
+import moment from 'moment';
+
 let debug: Function = () => {};
 let info: Function = () => {};
 let warn: Function = () => {};
 let error: Function = () => {};
 
+const timestamp = (): string =>
+  `[${moment().format('HH:mm:ss:SSS YYYY-MM-DD')}]`;
+
 /* eslint-disable */
 if (process.env.NODE_ENV === 'development') {
   debug = (...args) => {
-    console.log(Date.now(), ...args);
+    console.log(timestamp(), ...args);
   };
   info = (...args) => {
-    console.info(Date.now(), ...args);
+    console.info(timestamp(), ...args);
   };
   warn = (...args) => {
-    console.warn(Date.now(), ...args);
+    console.warn(timestamp(), ...args);
   };
   error = (...args) => {
-    console.error(Date.now(), ...args);
+    console.error(timestamp(), ...args);
   };
 }
 /* eslint-enable */
