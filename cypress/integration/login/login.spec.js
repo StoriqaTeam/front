d44a3b10a95c08fa97509cc52a71d9eacc32b73c
Levenets Aleksey <a.levenec@storiqa.com>
2018-11-16 17:20:36 +0300

    add global cookie in cypress for clusters

--- cypress/integration/login/login.spec.js
+++ cypress/integration/login/login.spec.js
@@ -2,8 +2,14 @@
 
 describe('Login page', () => {
   it('exists', () => {
-    cy.setCookie('holyshit', 'iamcool');
     cy.visit('/login');
-    cy.get;
+    cy.get('[data-test=email]')
+      .type('il.ya@sald.in')
+      .should('have.value', 'il.ya@sald.in');
+    cy.get('[data-test=password]')
+      .type('1q2w#E$R')
+      .should('have.value', '1q2w#E$R');
+    cy.get('[data-test=signInButton]').click();
+    cy.url().should('equals', `${Cypress.config('baseUrl')}/`);
   });
 });
