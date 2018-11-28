285bac08e7286a19e840455e1aa745dcc89d87c4
Аневризма <aneurysmjs@outlook.com>
2018-11-28 12:42:07 +0300

    feat (Manage/Settings) display alert for max error file

--- src/pages/Manage/Store/Settings/Form.js
+++ src/pages/Manage/Store/Settings/Form.js
@@ -26,6 +26,7 @@ import { InputSlug } from 'components/common/InputSlug';
 import { withErrorBoundary } from 'components/common/ErrorBoundaries';
 import { UploadWrapper } from 'components/Upload';
 import { Icon } from 'components/Icon';
+import { withShowAlert } from 'components/Alerts/AlertContext';
 
 import { uploadFile, convertSrc } from 'utils';
 
@@ -268,7 +269,13 @@ class Form extends Component<PropsType, StateType> {
       .finally(() => {
         this.setState({ isMainPhotoUploading: false });
       })
-      .catch(alert);
+      .catch(error => {
+        this.props.showAlert({
+          type: 'danger',
+          text: error.message,
+          link: { text: 'Close.' },
+        });
+      });
   };
 
   handleDeleteCover = () => {
@@ -437,4 +444,4 @@ Form.contextTypes = {
 };
 
 // $FlowIgnoreMe
-export default withErrorBoundary(Form);
+export default withErrorBoundary(withShowAlert(Form));
