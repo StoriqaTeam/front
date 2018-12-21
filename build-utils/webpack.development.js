01b6bde56e6013e8bc9403f9e0d55f620cd97675
Аневризма <aneurysmjs@outlook.com>
2018-12-21 15:08:59 +0300

    chore (Webpack) exclude mini-css-extract-plugin fromon HardSourceWebpackPlugin

--- build-utils/webpack.development.js
+++ build-utils/webpack.development.js
@@ -76,6 +76,16 @@ module.exports = (mode) => {
       new HtmlWebpackHarddiskPlugin({
         outputPath: path.resolve(__dirname, 'templates')
       }), 
+      new HardSourceWebpackPlugin.ExcludeModulePlugin([
+        {
+          // HardSource works with mini-css-extract-plugin but due to how
+          // mini-css emits assets, assets are not emitted on repeated builds with
+          // mini-css and hard-source together. Ignoring the mini-css loader
+          // modules, but not the other css loader modules, excludes the modules
+          // that mini-css needs rebuilt to output assets every time.
+          test: /mini-css-extract-plugin[\\/]dist[\\/]loader/,
+        },
+      ]),
       new HardSourceWebpackPlugin(),
       // for HMR
       new webpack.HotModuleReplacementPlugin(),
