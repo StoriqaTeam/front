b319264796ae3c60cc3b66a7e8329020735f6e1b
Levenets Aleksey <a.levenec@storiqa.com>
2018-12-27 11:07:09 +0300

    add categoriesWithProducts

--- src/routes/index.js
+++ src/routes/index.js
@@ -83,7 +83,7 @@ const routes = (
             isoCode
           }
           currencies
-          categories {
+          categoriesWithProducts {
             name {
               lang
               text
