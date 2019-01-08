ea6354e17cdaab295eed3033de6f53bc95b082cc
Levenets Aleksey <a.levenec@storiqa.com>
2019-01-08 16:58:42 +0300

    fix shop items in shop tab

---
+++ src/pages/Store/Showcase/i18n.js
@@ -0,0 +1,31 @@
+// @flow strict
+// @flow-runtime
+
+import { t } from 'translation/utils';
+import type { Translation } from 'translation/utils';
+
+type TranslationDicType = {|
+  mostPopular: string,
+  sale: string,
+|};
+type TranslationsBundleType = Translation<TranslationDicType>;
+
+const translations: TranslationsBundleType = {
+  en: {
+    mostPopular: 'Most Popular',
+    sale: 'Sale',
+  },
+};
+
+const validate = (json: {}, verbose: boolean = false): boolean => {
+  try {
+    (json: TranslationsBundleType); // eslint-disable-line
+    return true;
+  } catch (err) {
+    verbose && console.error(err); // eslint-disable-line
+    return false;
+  }
+};
+
+export { translations, validate };
+export default t(translations);
