4b4efe79be7c97dc9488bcc56f0659cb25534d7b
Аневризма <aneurysmjs@outlook.com>
2019-01-16 16:36:57 +0300

    fix (RichEditor) remove formats (#883)

--- src/components/RichEditor/RichEditor.js
+++ src/components/RichEditor/RichEditor.js
@@ -33,44 +33,6 @@ const menu = {
   tools: { title: 'Tools', items: 'spellchecker code' },
 };
 
-const formats = {
-  alignleft: {
-    selector: 'p, h1, h2, h3, h4, h5, h6, td, th, div, ul, ol, li, table, img',
-    classes: 'left',
-  },
-  aligncenter: {
-    selector: 'p, h1, h2, h3, h4, h5, h6, td, th, div, ul, ol, li, table, img',
-    classes: 'center',
-  },
-  alignright: {
-    selector: 'p, h1, h2, h3, h4, h5, h6, td, th, div, ul, ol, li, table, img',
-    classes: 'right',
-  },
-  alignjustify: {
-    selector: 'p, h1, h2, h3, h4, h5, h6, td, th, div, ul, ol, li, table, img',
-    classes: 'full',
-  },
-  bold: { inline: 'span', classes: 'bold' },
-  italic: { inline: 'span', classes: 'italic' },
-  underline: { inline: 'span', classes: 'underline', exact: true },
-  strikethrough: { inline: 'del' },
-  forecolor: {
-    inline: 'span',
-    classes: 'forecolor',
-    styles: { color: '%value' },
-  },
-  hilitecolor: {
-    inline: 'span',
-    classes: 'hilitecolor',
-    styles: { backgroundColor: '%value' },
-  },
-  custom_format: {
-    block: 'h1',
-    attributes: { title: 'Header' },
-    styles: { color: 'red' },
-  },
-};
-
 class RichEditor extends Component<PropsType> {
   static defaultProps = {
     onChange: () => {},
@@ -92,7 +54,6 @@ class RichEditor extends Component<PropsType> {
           initialValue={!isEmpty(content) ? content : '<p>Write something</p>'}
           onChange={this.handleEditorChange}
           init={{
-            formats,
             menubar: false,
             branding: false,
             min_height: 400,
