e3bddb76ed3108b6d37ff47704e8ba21ac1044b6
Аневризма <aneurysmjs@outlook.com>
2019-01-30 11:56:58 +0300

    feat (pages/Cart/CartProduct) add confirmation popup

---
+++ src/pages/BuyNow/CartStore/i18n.js
@@ -0,0 +1,36 @@
+// @flow strict
+// @flow-runtime
+
+import { t } from 'translation/utils';
+import type { Translation } from 'translation/utils';
+
+type TranslationDicType = {|
+  confirmationDescription: string,
+  deleteYourProduct: string,
+  confirmText: string,
+  cancelText: string,
+|};
+type TranslationsBundleType = Translation<TranslationDicType>;
+
+const translations: TranslationsBundleType = {
+  en: {
+    confirmationDescription:
+      'Are you sure you want to delete this listing? All the listing information will be discarded and cannot be retrieved.',
+    deleteYourProduct: 'Delete your product?',
+    confirmText: 'Yes, delete, please',
+    cancelText: 'Cancel',
+  },
+};
+
+const validate = (json: {}, verbose: boolean = false): boolean => {
+  try {
+    (json: TranslationsBundleType); // eslint-disable-line
+    return true;
+  } catch (err) {
+    verbose && console.error(err); // eslint-disable-line
+    return false;
+  }
+};
+
+export { translations, validate };
+export default t(translations);
